112ca8da5307e96e6b6f18eecc6d109b9148f437
Vinalyyy <copynamever@gmail.com>
2026-04-22 13:46:17 +0000

    v0.11.18

--- pages/wafflStack.js
+++ pages/wafflStack.js
@@ -6,6 +6,11 @@ function orderWaffl() {
     waffl.textContent = "Waffl";
 
     container.appendChild(waffl);
+
+    orderCount ++;
+
+    const counter = document.getElementById("orderCounter");
+    counter.innerHTML = orderCount;
 }
 function eatWaffl() {
     const container = document.getElementById("plate");
@@ -13,10 +18,18 @@ function eatWaffl() {
     if (container.lastElementChild != null) {
         container.removeChild(container.lastElementChild);
     }
+
+    eatenCount ++;
+
+    const counter = document.getElementById("eatenCounter");
+    counter.innerHTML = eatenCount;
 }
 
 const orderButton = document.getElementById("orderButton");
 const eatButton = document.getElementById("eatButton");
+
+let orderCount = 0;
+let eatenCount = 0;
 
 orderButton.addEventListener("click", orderWaffl);
 eatButton.addEventListener("click", eatWaffl);
